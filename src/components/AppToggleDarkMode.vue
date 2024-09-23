b1112c801ede7f18664a658dba3f03a50beb1cfc
leonardo Cueto <leocueto1999@gmail.com>
2024-09-23 20:28:55 -0300

    New portfolio version

---
+++ src/components/AppToggleDarkMode.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="p-2 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-300 rounded-full transition-all hover:scale-110"
+    @click="toggleDarkMode()"
+  >
+    <component
+      :is="iconComponent"
+      :key="iconComponent"
+      color="gray"
+      :size="25"
+    />
+  </button>
+</template>
+<script lang="ts" setup>
+import { IconSun, IconMoon } from "@tabler/icons-vue";
+import { computed, ref } from "vue";
+
+const darkMode = ref(false);
+const iconComponent = computed(() => (darkMode.value ? IconSun : IconMoon));
+const toggleDarkMode = () => {
+  const element = document.querySelector("html");
+  element?.classList.toggle("dark");
+  darkMode.value = !darkMode.value;
+};
+</script>
